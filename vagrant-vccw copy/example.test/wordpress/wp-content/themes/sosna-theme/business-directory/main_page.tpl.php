21ce0c2cc9011bb8138785a649c42bbda1a1a7c9
kyle <kyledr215@gmail.com>
2019-07-24 11:17:32 -0400

    main wordpress page configured

--- vagrant-vccw copy/example.test/wordpress/wp-content/themes/sosna-theme/business-directory/main_page.tpl.php
+++ vagrant-vccw copy/example.test/wordpress/wp-content/themes/sosna-theme/business-directory/main_page.tpl.php
@@ -16,25 +16,22 @@
  <h2 class="prompt">What would you like to do today?</h2>
 
    <div class="business-categories">
-     <div class="link-column">
-       <!-- pulls in business categories -->
-       <?php echo do_shortcode('[businessdirectory-categories]'); ?>
-     </div>
-     <div class="link-column">
-       <ul>
-         <li class="decription-box"> <h4>Local Arts and Entertainment bussinesses within the Sosna region</h4> </li>
-         <li class="decription-box"> <h4>Local Automotive, Contractors and Construction Materials bussinesses within the Sosna region</h4> </li>
-         <li class="decription-box"> <h4>Local Hair and Nail bussinesses within the Sosna region</h4> </li>
-         <li class="decription-box"> <h4>Local Medical, Health, Wellness and Fitness bussinesses within the Sosna region</h4> </li>
-         <li class="decription-box"> <h4>Parks, School, Banks, Drycleaner/Laundromat, Houses of Worship, Senior Services, Professional Services, Legal, Kids, Pets, Parks & Recreation, Parking</h4> </li>
-         <li class="decription-box"> <h4>Local Restaurants, Food & Drink, Grocery / Convenience Stores bussinesses within the Sosna region</h4> </li>
-         <li class="decription-box"> <h4>Local stores and shopping bussinesses in the sosna region</h4> </li>
-       </ul>
-     </div>
 
+      <div class="category">
+        <!-- pulls in business categories -->
+        <?php echo do_shortcode('[businessdirectory-categories]'); ?>
+      </div>
 
-
-  </div>
+       <div class="description">
+           <li class="description-box"> <h4>Local Arts and Entertainment bussinesses within the Sosna region</h4> </li>
+           <li class="description-box"> <h4>Local Automotive, Contractors and Construction Materials bussinesses within the Sosna region</h4> </li>
+           <li class="description-box"> <h4>Local Restaurants, Food & Drink, Grocery / Convenience Stores bussinesses within the Sosna region</h4> </li>
+           <li class="description-box"> <h4>Local Hair and Nail bussinesses within the Sosna region</h4> </li>
+           <li class="description-box"> <h4>Local Medical, Health, Wellness and Fitness bussinesses within the Sosna region</h4> </li>
+           <li class="description-box"> <h4>Parks, School, Banks, Drycleaner/Laundromat, Houses of Worship, Senior Services, Professional Services, Legal, Kids, Pets, Parks & Recreation, Parking </h4> </li>
+           <li class="description-box"> <h4>Local stores and shopping bussinesses in the sosna region</h4> </li>
+       </div>
+   </div>
 
 
 
